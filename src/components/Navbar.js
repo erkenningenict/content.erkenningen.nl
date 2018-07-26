a141b2ac892af6408e81e1c940740de0f624dc48
Didier Hartong <info@focuswider.com>
2018-07-26 20:17:48 +0200

    feat(navbar): redesign based on bootstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "gatsby-link";
 import logo from "../img/BE-logo.svg";
 // import classNames from "classnames";
-import Menu, { SubMenu, Item as MenuItem, Divider } from 'rc-menu';
+import Menu, { SubMenu, Item as MenuItem, Divider } from "rc-menu";
 
 // import { CSSTransition, TransitionGroup } from "react-transition-group";
 // import CSSTransition from "react-transition-group/CSSTransition";
@@ -13,7 +13,7 @@ export default class Navbar extends React.Component {
     super(props);
 
     this.state = {
-      collapsed: true,
+      collapsed: true
     };
     // this.toggleNavbar = this.toggleNavbar.bind(this);
     // this.showMenu = this.showMenu.bind(this);
@@ -27,73 +27,172 @@ export default class Navbar extends React.Component {
   }
   render() {
     return (
-      <div className="container">
+      <nav className="container navbar-be">
         <div className="row">
-          <div className="col">
-            <nav className="navbar navbar-expand-lg navbar-be">
-              <Link to="/" className="navbar-brand">
-                <img
-                  src={logo}
-                  alt="Bureau Erkenningen"
-                  style={{ width: "233px" }}
-                />
-              </Link>
-              <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-                onClick={this.toggleNavbar}
-              >
-                <span className="navbar-toggler-icon" />
-              </button>
+          <div className="col-md-3 pr-0">
+            <Link to="/" className="navbar-brand">
+              <img
+                src={logo}
+                alt="Bureau Erkenningen"
+                style={{ width: "233px" }}
+              />
+            </Link>
+          </div>
+          <div className="col-md-9 pl-0 pt-5 pb-2 d-flex justify-content-end">
+            {/*<button*/}
+            {/*className="navbar-toggler"*/}
+            {/*type="button"*/}
+            {/*data-toggle="collapse"*/}
+            {/*data-target="#navbarSupportedContent"*/}
+            {/*aria-controls="navbarSupportedContent"*/}
+            {/*aria-expanded="false"*/}
+            {/*aria-label="Toggle navigation"*/}
+            {/*onClick={this.toggleNavbar}*/}
+            {/*>*/}
+            {/*<span className="navbar-toggler-icon"/>*/}
+            {/*</button>*/}
 
-              <Menu mode={'horizontal'} className="navbar-nav mr-auto">
-                <MenuItem><Link activeClassName="active" exact to="/">Home</Link></MenuItem>
-                <SubMenu title={<Link activeClassName="active" to="/bureau-erkenningen">Bureau Erkenningen</Link>}>
-                  <MenuItem><Link to="/bureau-erkenningen/licentiehouders">Licentiehouders</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/kennisaanbieders">Kennisaanbieders</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/exameninstellingen">Exameninstellingen</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">KBA-GB gecertificeerde bedrijven</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/formulieren">Formulieren</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/brochures">Brochures</Link></MenuItem>
-                </SubMenu>
-                <SubMenu title={<Link activeClassName="active" to="/licenties">Licenties</Link>}>
-                  <MenuItem><Link to="/bureau-erkenningen/licentiehouders">Welke licenties zijn er?</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/kennisaanbieders">Veiligheidsinstructies</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/exameninstellingen">Welke licentie heb ik nodig?</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">Licentie aanvragen</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">Licentie verlengen</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">KBA-GB bijschrijven</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">Wetten en regels</Link></MenuItem>
-                </SubMenu>
-                <SubMenu title={<Link activeClassName="active" to="/bijeenkomsten">Bijeenkomsten</Link>}>
-                  <MenuItem><Link to="/bureau-erkenningen/licentiehouders">Bijeenkomsten zoeken</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/kennisaanbieders">Bijeenkomsten organiseren</Link></MenuItem>
-                </SubMenu>
-                <SubMenu title={<Link activeClassName="active" to="/bijeenkomsten">Mijn Bureau Erkenningen</Link>}>
-                  <MenuItem><Link to="/bureau-erkenningen/licentiehouders">Licentiehouders</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/kennisaanbieders">Kennisaanbieders</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/exameninstellingen">Exameninstellingen</Link></MenuItem>
-                  <MenuItem><Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">KBA-GB gecertificeerde bedrijven</Link></MenuItem>
-                </SubMenu>
-              </Menu>
-                <form className="form-inline">
-                  <input
-                    className="form-control search-input"
-                    type="search"
-                    placeholder="Zoek op trefwoord"
-                    aria-label="Search"
-                  />
-                </form>
-            </nav>
+            <Menu mode={"horizontal"} className="mr-4">
+              <MenuItem>
+                <Link activeClassName="active" exact to="/">
+                  Home
+                </Link>
+              </MenuItem>
+              <SubMenu
+                title={
+                  <Link activeClassName="active" to="/bureau-erkenningen">
+                    Bureau Erkenningen
+                  </Link>
+                }
+              >
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/licentiehouders">
+                    Licentiehouders
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/kennisaanbieders">
+                    Kennisaanbieders
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/exameninstellingen">
+                    Exameninstellingen
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">
+                    KBA-GB gecertificeerde bedrijven
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/formulieren">Formulieren</Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/brochures">Brochures</Link>
+                </MenuItem>
+              </SubMenu>
+              <SubMenu
+                title={
+                  <Link activeClassName="active" to="/licenties">
+                    Licenties
+                  </Link>
+                }
+              >
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/licentiehouders">
+                    Welke licenties zijn er?
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/kennisaanbieders">
+                    Veiligheidsinstructies
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/exameninstellingen">
+                    Welke licentie heb ik nodig?
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">
+                    Licentie aanvragen
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">
+                    Licentie verlengen
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">
+                    KBA-GB bijschrijven
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">
+                    Wetten en regels
+                  </Link>
+                </MenuItem>
+              </SubMenu>
+              <SubMenu
+                title={
+                  <Link activeClassName="active" to="/bijeenkomsten">
+                    Bijeenkomsten
+                  </Link>
+                }
+              >
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/licentiehouders">
+                    Bijeenkomsten zoeken
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/kennisaanbieders">
+                    Bijeenkomsten organiseren
+                  </Link>
+                </MenuItem>
+              </SubMenu>
+              <SubMenu
+                title={
+                  <Link activeClassName="active" to="/bijeenkomsten">
+                    Mijn Bureau Erkenningen
+                  </Link>
+                }
+              >
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/licentiehouders">
+                    Licentiehouders
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/kennisaanbieders">
+                    Kennisaanbieders
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/exameninstellingen">
+                    Exameninstellingen
+                  </Link>
+                </MenuItem>
+                <MenuItem>
+                  <Link to="/bureau-erkenningen/KBA-GB-gecertificeerde-bedrijven">
+                    KBA-GB gecertificeerde bedrijven
+                  </Link>
+                </MenuItem>
+              </SubMenu>
+            </Menu>
+            <form className="form-inline">
+              <input
+                className="form-control search-input"
+                type="search"
+                placeholder="Zoek op trefwoord"
+                aria-label="Search"
+              />
+            </form>
           </div>
         </div>
-      </div>
+      </nav>
     );
   }
 }
-
--- src/templates/page.js
+++ src/templates/page.js
@@ -8,49 +8,49 @@ import Breadcrumb from "./../components/Breadcrumb";
 export default ({ data }) => {
   const page = data.markdownRemark;
 
-  return (
-    <div>
-      {page.frontmatter.title && (
-        <Helmet>
-          <title>Erkenningen | {page.frontmatter.title}</title>
-        </Helmet>
-      )}
+  return <div>
+    {page.frontmatter.title && (
+      <Helmet>
+        <title>Erkenningen | {page.frontmatter.title}</title>
+      </Helmet>
+    )}
 
-      {page.frontmatter.title &&
-        page.frontmatter.excerpt && (
-          <Helmet>
-            <meta property="og:title" content={page.frontmatter.title} />
-            <meta name="twitter:title" content={page.frontmatter.title} />
+    {page.frontmatter.title &&
+    page.frontmatter.excerpt && (
+      <Helmet>
+        <meta property="og:title" content={page.frontmatter.title}/>
+        <meta name="twitter:title" content={page.frontmatter.title}/>
 
-            <meta
-              property="og:description"
-              content={page.frontmatter.excerpt}
-            />
-            <meta
-              name="twitter:description"
-              content={page.frontmatter.excerpt}
-            />
-          </Helmet>
-        )}
+        <meta
+          property="og:description"
+          content={page.frontmatter.excerpt}
+        />
+        <meta
+          name="twitter:description"
+          content={page.frontmatter.excerpt}
+        />
+      </Helmet>
+    )}
 
-      <div className="navbar navbar-expand-lg navbar-be__breadcrumbs-container">
-        <div className="navbar-be__breadcrumbs-spacer">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3 navbar-be__breadcrumbs-spacer">
           {" ".replace(/ /g, "\u00a0")}
         </div>
-        <Breadcrumb absolutePath={page.fileAbsolutePath} />
+        <Breadcrumb absolutePath={page.fileAbsolutePath}/>
       </div>
       <div className="row">
-            <div className="col-md-3">
-Search
-            </div>
-            <div className="col-md-9">
-              <h1>{page.frontmatter.title}</h1>
-              <TextPageBody htmlAst={page.htmlAst} />
-            </div>
-            </div>
-
+        <div className="col-md-3">
+          Search
+        </div>
+        <div className="col-md-9 pl-0">
+          <h1>{page.frontmatter.title}</h1>
+          <TextPageBody htmlAst={page.htmlAst}/>
+        </div>
+      </div>
     </div>
-  );
+
+  </div>;
 };
 
 export const query = graphql`
