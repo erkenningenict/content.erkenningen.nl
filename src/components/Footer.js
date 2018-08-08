c7e34076e2b6712ac0701ccaff4de5c065c54dbd
Didier Hartong <info@focuswider.com>
2018-08-08 20:38:43 +0200

    feat(footer): responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,19 +4,19 @@ import Link from "gatsby-link";
 const Footer = () => (
   <div className="container">
     <div className="row">
-      <div className="col footer">
+      <div className="col footer d-flex flex-wrap my-2  d-flex justify-content-around">
         <Link to="/contact" className="footerLink">
           Contact
         </Link>
-        |
+        <div className="d-none d-sm-block">|</div>
         <Link to="/faq" className="footerLink">
           FAQ
         </Link>
-        |
+        <div className="d-none d-sm-block">|</div>
         <a href="https://erkenningen.nl/" className="footerLink">
           Disclaimer
         </a>
-        |
+        <div className="d-none d-sm-block">|</div>
         <a
           href="https://www.erkenningen.nl/Default.aspx?tabid=289"
           className="footerLink"
