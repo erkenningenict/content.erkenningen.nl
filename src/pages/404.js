1d89fc49964237f3fad486eb040e398b10187344
Gerwin Koudijs <gerwin.koudijs@gmail.com>
2019-06-28 14:48:07 +0200

    style: fixed formatting

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,23 +4,23 @@ import Layout from '../components/layout';
 
 const NotFoundPage = () => (
   <Layout>
-  <div className="container">
-    <div className="row mb-5">
-      <div className="col-md-1 navbar-be__breadcrumbs-spacer-orange" />
-      <div className="col-md-2 navbar-be__breadcrumbs-spacer-green" />
-      <div className="col-md-9 navbar-be__breadcrumbs-container">Niet gevonden</div>
-    </div>
-    <div className="row">
-      <div className="col-md-3 pr-5">
-        <FaqSidebar />
+    <div className="container">
+      <div className="row mb-5">
+        <div className="col-md-1 navbar-be__breadcrumbs-spacer-orange" />
+        <div className="col-md-2 navbar-be__breadcrumbs-spacer-green" />
+        <div className="col-md-9 navbar-be__breadcrumbs-container">Niet gevonden</div>
       </div>
-      <div className="col-md-9 pl-0">
-        <h1>Pagina niet gevonden</h1>
-        <p>Wat jammer, de gezocht pagina bestaat niet. Kies uit het menu een pagina.</p>
+      <div className="row">
+        <div className="col-md-3 pr-5">
+          <FaqSidebar />
+        </div>
+        <div className="col-md-9 pl-0">
+          <h1>Pagina niet gevonden</h1>
+          <p>Wat jammer, de gezocht pagina bestaat niet. Kies uit het menu een pagina.</p>
+        </div>
       </div>
     </div>
-    </div>
-    </Layout>
+  </Layout>
 );
 
 export default NotFoundPage;
