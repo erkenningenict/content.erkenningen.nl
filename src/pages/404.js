948c24ac6677902e9d2fcfa2c1d3d3d4ea610de9
Gerwin Koudijs <gerwin.koudijs@gmail.com>
2019-07-15 09:11:07 +0200

    feat: added 404 handling for old admin links

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,47 @@
 import React from 'react';
+import { ERKENNINGEN_ADMIN_URL } from '@erkenningen/config';
 import FaqSidebar from '../components/FaqSidebar';
 import Layout from '../components/layout';
 
-const NotFoundPage = () => (
-  <Layout>
-    <div className="container">
-      <div className="row mb-5">
-        <div className="col-md-1 navbar-be__breadcrumbs-spacer-orange" />
-        <div className="col-md-2 navbar-be__breadcrumbs-spacer-green" />
-        <div className="col-md-9 navbar-be__breadcrumbs-container">Niet gevonden</div>
-      </div>
-      <div className="row">
-        <div className="col-md-3 pr-5">
-          <FaqSidebar />
+const NotFoundPage = () => {
+  // Determine if original request was a link to the old site
+  // If so, then show warning and add option to redirect to the administration site
+  const isOldLink = () => {
+    return typeof window !== 'undefined' && window.location.pathname.includes('/Default.aspx');
+  };
+  const getOldPath = () => {
+    return typeof window !== 'undefined' && window.location.pathname + window.location.search;
+  };
+
+  return (
+    <Layout>
+      <div className="container">
+        <div className="row mb-5">
+          <div className="col-md-1 navbar-be__breadcrumbs-spacer-orange" />
+          <div className="col-md-2 navbar-be__breadcrumbs-spacer-green" />
+          <div className="col-md-9 navbar-be__breadcrumbs-container">Niet gevonden</div>
         </div>
-        <div className="col-md-9 pl-0">
-          <h1>Pagina niet gevonden</h1>
-          <p>Wat jammer, de gezocht pagina bestaat niet. Kies uit het menu een pagina.</p>
+        <div className="row">
+          <div className="col-md-3 pr-5">
+            <FaqSidebar />
+          </div>
+          <div className="col-md-9 pl-0">
+            <h1>Pagina niet gevonden</h1>
+            {isOldLink() ? (
+              <p>
+                De pagina die u probeerde op te vragen is verhuisd naar de administratiesite van
+                Bureau Erkenningen en is mogelijk niet meer beschikbaar. Klik{' '}
+                <a href={ERKENNINGEN_ADMIN_URL + getOldPath()}>hier</a> om de pagina te openen in de
+                administratiesite of blijf op deze informatiesite.
+              </p>
+            ) : (
+              <p>Wat jammer, de gezocht pagina bestaat niet. Kies uit het menu een pagina.</p>
+            )}
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
