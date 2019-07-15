8ad90f9386186645d022bcf57121c90d3610f5f4
Gerwin Koudijs <gerwin.koudijs@gmail.com>
2019-07-15 09:22:49 +0200

    style: add alert wrapper to 404 page and added more info

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ERKENNINGEN_ADMIN_URL } from '@erkenningen/config';
 import FaqSidebar from '../components/FaqSidebar';
 import Layout from '../components/layout';
+import { Alert } from '@erkenningen/ui';
 
 const NotFoundPage = () => {
   // Determine if original request was a link to the old site
@@ -27,16 +28,19 @@ const NotFoundPage = () => {
           </div>
           <div className="col-md-9 pl-0">
             <h1>Pagina niet gevonden</h1>
-            {isOldLink() ? (
-              <p>
-                De pagina die u probeerde op te vragen is verhuisd naar de administratiesite van
-                Bureau Erkenningen en is mogelijk niet meer beschikbaar. Klik{' '}
-                <a href={ERKENNINGEN_ADMIN_URL + getOldPath()}>hier</a> om de pagina te openen in de
-                administratiesite of blijf op deze informatiesite.
-              </p>
-            ) : (
-              <p>Wat jammer, de gezocht pagina bestaat niet. Kies uit het menu een pagina.</p>
-            )}
+            <Alert type="warning">
+              {isOldLink() ? (
+                <>
+                  De pagina die u probeerde op te vragen is verhuisd naar de administratiesite van
+                  Bureau Erkenningen en is mogelijk niet meer beschikbaar. Klik{' '}
+                  <a href={ERKENNINGEN_ADMIN_URL + getOldPath()}>hier</a> om de pagina te openen in
+                  de administratiesite of blijf op deze informatiesite. En werk uw bladwijzers of
+                  favorieten bij.
+                </>
+              ) : (
+                <>Wat jammer, de gezocht pagina bestaat niet. Kies uit het menu een pagina.</>
+              )}
+            </Alert>
           </div>
         </div>
       </div>
